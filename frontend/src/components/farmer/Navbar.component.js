ebfc10f5a8a06c933c72948bf819f5dba7a90f5d
Saakshi Shah <shahsaakshi25@gmail.com>
2020-08-20 21:33:34 +0530

    Adding nav and product pages

---
+++ frontend/src/components/farmer/Navbar.component.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+
+export default class Navbar extends Component{
+    render(){
+        return(
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <Link to="/" className="navbar-brand">Gio Home Products</Link>
+            <div className="collpase navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+              <Link to="/" className="nav-link">Farmer</Link>
+              </li>
+              <li className="navbar-item">
+              <Link to="/farmer" className="nav-link">Farmer Product</Link>
+              </li>
+              <li className="navbar-item">
+              <Link to="/user" className="nav-link">Create User</Link>
+              </li>
+            </ul>
+            </div>
+          </nav>
+     
+        );
+    }
+}
+
