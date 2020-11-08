ab7969e1cdd2fb68a1ebde4ec2658d13977623f5
Sanket Shah <sanketyou8@gmail.com>
2020-11-08 16:25:33 +0530

    checl

--- frontend/src/screens/farmer/ProductList.js
+++ frontend/src/screens/farmer/ProductList.js
@@ -117,28 +117,9 @@ export default class ProductList extends Component{
                     </Fragment>
                 ))}
                 </div>
+            </div>
 
-                {/* <ul className="products">
-                    {this.state.products.map((u)=>(
-                    <li key={u._id}>
-                    <div className="product">
-                    {/* <Link to={'' + u._id}>
-                        <img
-                        className="product-image"
-                        src={u.image}
-                        alt="Product Image"
-                        height="200"
-                        width="200"
-                        />
-                    </Link> */}
-                    <div className="product-name">
-                        <Link to={'' + u._id}>{u.name}</Link>
-                    </div>
-                    {/* <div className="product-description">{u.description}</div>
-                    <div className="product-price">Price: {u.price}</div>
-                    <div className="product-life">Life: {u.life}</div> */}
-                    <button variant="btn btn-success" value={this.state.name} onClick={() =>this.handleChange(u)}>Add to list</button>
-                    </div>
+               
 
         );
     }
